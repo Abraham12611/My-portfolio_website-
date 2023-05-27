73c3508f8d8b38d302fd1907c6805d75c8bd8d3a
Abraham Dahunsi <113689862+Abraham12611@users.noreply.github.com>
2023-05-27 17:22:56 +0100

    Added js file

---
+++ index.js
@@ -0,0 +1,7 @@
+ var typed= new Typed(".text", {
+  strings:[" Front-end Developer", "Back-end Engineer", "Full-stack Developer", "DevOps engineer"],
+  typeSpeed:100,
+  backspeed:100,
+  backDelay:1000,
+  loop:true
+})
