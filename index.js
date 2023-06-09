c1f0ab6a6b97daa7a29dacdd38769873577de0a7
Abraham12611 <abraham.dahunsi@gmail.com>
2023-06-09 18:10:57 +0100

    Dark-Mode

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
   backDelay:1000,
   loop:true
 })
+
+var timeoutId;
+var icon = document.getElementById('icon');
+
+function hideIcon() {
+  icon.classList.remove('show');
+}
+
+function showIcon() {
+  icon.classList.add('show');
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(hideIcon, 5000);
+}
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) {
+    showIcon();
+  }
+});
+
+timeoutId = setTimeout(hideIcon, 5000);
